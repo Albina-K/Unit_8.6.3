94d64fa0d69b28f0b518d52458d3b28be4b3a918
Albina-K <kamalieva.albish@mail.ru>
2024-04-08 21:09:45 +0300

    Добавьте файлы проекта.

---
+++ Unit_8.6.3/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_8_6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                   
+            string path = @"C:\\User\\Luft\\SkillFactoryNew";
+            if (Directory.Exists(path))
+            {
+
+                try
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(path);
+                    Console.WriteLine($"В папке: {path}");
+
+                    Console.WriteLine($"Всего {DirectoryExtinsion.DirSize(dirInfo)} байт");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Произошла ошибка: {e}"); ; ;
+                }
+            }
+
+
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    var dirInfo = new DirectoryInfo(path);
+                    foreach (DirectoryInfo directoryInfo in dirInfo.GetDirectories())
+                    {
+                        directoryInfo.Delete(true);
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                }
+                try
+                {
+                    var dirInfo = new DirectoryInfo(path);
+                    foreach (FileInfo fileInfo in dirInfo.GetFiles())
+                    {
+                            fileInfo.Delete();
+
+                    }
+                   
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Папки не существует");
+            }
+
+ 
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                Console.WriteLine($"В папке: {path}");
+
+                Console.WriteLine($"Всего {DirectoryExtinsion.DirSize(dirInfo)} байт");
+                Console.WriteLine($"Папка {path} очищена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Произошла ошибка: {e}"); ; ;
+            }
+        }
+
+
+    }
+
+
+   
+
+    public static class DirectoryExtinsion
+    {
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dirs = d.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                size += DirSize(dir);
+            }
+            return size;
+        }
+    }
+}
